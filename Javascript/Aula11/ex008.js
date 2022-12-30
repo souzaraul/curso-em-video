ffc9f9a40b5a1e16d5c037c236def14d2895878f
Raul Souza <raulsilvasouza@hotmail.com>
2022-12-29 21:10:00 -0300

    aprendendo novas funções

---
+++ Javascript/Aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 7
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log(`você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança!`)
